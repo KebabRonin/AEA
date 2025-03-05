9132425383f0f5d5fe3a4d0ba2e11f1f91ab034c
KebabRonin <alrx.brss@gmail.com>
2025-03-05 18:10:41 +0200

    queens

--- H1/queens.py
+++ H1/queens.py
@@ -8,18 +8,29 @@ def get_queens_instance(n, blocked = []):
 	mdl.add(mdl.all_diff(lines))
 	mdl.add(mdl.all_diff(lines[i] + i for i in range(n)))
 	mdl.add(mdl.all_diff(lines[i] - i for i in range(n)))
-
+	for b in blocked:
+		mdl.add(lines[b[0]] != b[1])
 	return mdl
 
-def print_sol(msol: cplex.CpoSolveResult):
+def print_sol(msol: cplex.CpoSolveResult, blocked = []):
 	N = len(msol.get_all_var_solutions())
-	for line in msol.get_all_var_solutions():
-		line = line.get_value()
-		print('_ '*line + '+ ' + '_ '*(N - line - 1))
-
+	for line in range(N):
+		line = msol.get_var_solution(f"lines_{line}").get_value()
+		b = [b[1] for b in blocked if b[0] == line]
+		for j in range(N):
+			if j in b:
+				print("X ", end = "")
+			elif j == line:
+				print("Q ", end = "")
+			else:
+				print("_ ", end = "")
+		print("")
 
 N = 4
-mdl = get_queens_instance(N)
+# Blocked: list of (line, column)
+blocked = [(0, 1), (0, 3)]
+mdl = get_queens_instance(N, blocked)
 msol = mdl.solve()
 print(msol.get_solve_status())
-print_sol(msol)
+if msol.get_solve_status() == "Feasible":
+	print_sol(msol, blocked)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Echipa Brassat Alexandru (IAO1) & Drumia Petru-Sebastian (IAO1)
+Echipa Brassat Alexandru (IAO1) & Drumia Petru-Sebastian (IAO1) & Teodorescu Calin (SD1)
 
 Proiect: [Hitting Set](https://pacechallenge.org/2025/hs/)
